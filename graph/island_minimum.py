712d5dd63e0131b7e60bc29c3d4cdcd91242c836
tutRPi <kontakt@tutorials-raspberrypi.de>
2022-02-22 21:54:39 +0100

    added island path algorithms

---
+++ graph/island_minimum.py
@@ -0,0 +1,34 @@
+def island_minimum(grid) -> int:
+    minimum = -1
+    discovered = set({})
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
+            if (row, col) not in discovered and grid[row][col] == 1:
+                count = 0
+                queue = [(row, col)]
+                while len(queue) > 0:
+                    n_r, n_c = queue.pop(0)
+                    discovered.add((n_r, n_c))
+                    # top, right, bottom, left
+                    for r, c in [(n_r - 1, n_c), (n_r, n_c + 1), (n_r + 1, n_c), (n_r, n_c - 1)]:
+                        if 0 <= r < len(grid) and 0 <= c < len(grid[r]):
+                            if grid[r][c] == 1 and (r, c) not in discovered:
+                                queue.append((r, c))
+                                count += 1
+                if minimum == -1 or count < minimum:
+                    minimum = count
+    return count
+
+
+if __name__ == "__main__":
+    # we have a n x m map with water/land.
+    # land is connected left/right/up/down
+    grid = [
+        [0, 1, 0, 0, 1, 0],
+        [1, 1, 0, 0, 1, 0],
+        [0, 1, 0, 0, 0, 0],
+        [0, 0, 0, 1, 1, 0],
+        [0, 1, 0, 1, 1, 0],
+        [0, 0, 0, 0, 0, 0],
+    ]
+    assert island_minimum(grid) == 2
