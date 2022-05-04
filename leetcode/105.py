273d2e68d9f4a29d93bccb66147f9f9097001218
tutRPi <kontakt@tutorials-raspberrypi.de>
2022-05-05 00:03:39 +0200

    added leetcode solution

---
+++ leetcode/105.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        if not preorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        index_inorder_root = inorder.index(preorder[0])
+
+        root.left = self.buildTree(preorder[1:index_inorder_root + 1], inorder[:index_inorder_root])
+        root.right = self.buildTree(preorder[index_inorder_root + 1:], inorder[index_inorder_root + 1:])
+
+        return root
