c658a0bf6dcdf6ca25b134127264738a34aa40c8
tutRPi <kontakt@tutorials-raspberrypi.de>
2022-06-04 02:43:30 +0200

    added leetcode solution

---
+++ leetcode/1509.py
@@ -0,0 +1,13 @@
+class Solution:
+    def minDifference(self, nums: List[int]) -> int:
+        if len(nums) <= 3:
+            return 0
+        nums.sort()
+
+        options = [
+            nums[-1] - nums[3],
+            nums[-2] - nums[2],
+            nums[-3] - nums[1],
+            nums[-4] - nums[0],
+        ]
+        return min(options)
