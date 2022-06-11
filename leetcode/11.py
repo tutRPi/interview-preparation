62d28174171bce0d699cb12f89c319a20180a487
tutRPi <kontakt@tutorials-raspberrypi.de>
2022-06-12 00:18:28 +0200

    added leetcode solution

--- leetcode/11.py
+++ leetcode/11.py
@@ -1,19 +1,13 @@
 class Solution:
-
     def maxArea(self, height: List[int]) -> int:
-
-        current_area = 0
+        res = 0
         left, right = 0, len(height) - 1
-
-        while left != right:
-
-            area = (right - left) * min(height[left], height[right])
-            if area > current_area:
-                current_area = area
+        while left < right:
+            res = max(res, min(height[left], height[right]) * (right - left))
 
             if height[left] < height[right]:
                 left += 1
             else:
                 right -= 1
 
-        return current_area
+        return res
