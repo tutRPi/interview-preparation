7dd84b44c681ffcefd3b1bbb03bb6eadda2deb2a
tutRPi <kontakt@tutorials-raspberrypi.de>
2022-11-08 23:08:21 +0100

    added solution

---
+++ fbrecruiting/Minimizing Permutations.py
@@ -0,0 +1,67 @@
+import math
+
+
+# Add any extra import statements you may need here
+
+
+# Add any helper functions you may need here
+
+
+def minOperations(arr):
+    # Write your code here
+
+    count = 0
+    goal = list(range(1, len(arr) + 1))
+    for i in range(len(arr)):
+        if arr[i] == goal[i]:
+            continue
+        pos = i + 1
+        while arr[pos] != goal[i]:
+            pos += 1
+        arr[i: pos + 1] = arr[i: pos + 1][::-1]
+        count += 1
+    return count
+
+
+# These are the tests we use to determine if the solution is correct.
+# You can add your own at the bottom.
+
+def printInteger(n):
+    print('[', n, ']', sep='', end='')
+
+
+test_case_number = 1
+
+
+def check(expected, output):
+    global test_case_number
+    result = False
+    if expected == output:
+        result = True
+    rightTick = '\u2713'
+    wrongTick = '\u2717'
+    if result:
+        print(rightTick, 'Test #', test_case_number, sep='')
+    else:
+        print(wrongTick, 'Test #', test_case_number, ': Expected ', sep='', end='')
+        printInteger(expected)
+        print(' Your output: ', end='')
+        printInteger(output)
+        print()
+    test_case_number += 1
+
+
+if __name__ == "__main__":
+    n_1 = 5
+    arr_1 = [1, 2, 5, 4, 3]
+    expected_1 = 1
+    output_1 = minOperations(arr_1)
+    check(expected_1, output_1)
+
+    n_2 = 3
+    arr_2 = [3, 1, 2]
+    expected_2 = 2
+    output_2 = minOperations(arr_2)
+    check(expected_2, output_2)
+
+    # Add your own test cases here
